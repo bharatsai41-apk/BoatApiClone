f8abba852ab4bd57c9d239d17713dc3a70625d3b
bharat sai <bharatsaimallina@gmail.com>
2026-06-25 17:18:30 +0530

    Delete Models directory

--- Models/UserOrders.cs
+++
@@ -1,8 +0,0 @@
-namespace boatapi.Models;
-
-public class UserOrders
-{
-    public required Guid OrderId { get; set; } = Guid.NewGuid();
-    public string? Trackingurl { get; set; }
-    public ICollection<OrderProducts> orderProducts { get; set; } = new List<OrderProducts>();
-}
